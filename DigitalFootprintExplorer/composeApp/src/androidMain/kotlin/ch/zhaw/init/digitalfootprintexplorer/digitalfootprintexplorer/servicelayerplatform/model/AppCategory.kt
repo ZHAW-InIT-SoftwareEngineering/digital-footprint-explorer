a643d8ad39843cbbcacc467c65422b4591090766
Atilla Kolali <kolalmeh@students.zhaw.ch>
2026-03-19 14:37:30 +0100

    feat: added app category (#35)

---
+++ DigitalFootprintExplorer/composeApp/src/androidMain/kotlin/ch/zhaw/init/digitalfootprintexplorer/digitalfootprintexplorer/servicelayerplatform/model/AppCategory.kt
@@ -0,0 +1,18 @@
+package ch.zhaw.init.digitalfootprintexplorer.digitalfootprintexplorer.servicelayerplatform.model
+
+/**
+ * The categories of apps.
+ * Defined in report and issue: https://github.zhaw.ch/BA-PA-wahl/DigitalFootprintExplorer/issues/35
+ */
+enum class AppCategory {
+    VIDEO_STREAMING,
+    AUDIO_STREAMING,
+    SOCIAL_MEDIA,
+    MESSAGING,
+    ARTIFICIAL_INTELLIGENCE,
+    E_MAIL,
+    VIDEO_CALL,
+    GAMING,
+    NAVIGATION,
+    MISCELLANEOUS
+}

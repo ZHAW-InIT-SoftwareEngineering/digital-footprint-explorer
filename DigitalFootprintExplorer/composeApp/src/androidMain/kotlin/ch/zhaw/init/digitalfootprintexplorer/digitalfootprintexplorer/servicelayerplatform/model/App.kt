ee0746cf1f8aa8bb3bbd50920d7995de6a97dc12
Atilla Kolali <kolalmeh@students.zhaw.ch>
2026-03-20 14:08:56 +0100

    feat: added app model (#35)

---
+++ DigitalFootprintExplorer/composeApp/src/androidMain/kotlin/ch/zhaw/init/digitalfootprintexplorer/digitalfootprintexplorer/servicelayerplatform/model/App.kt
@@ -0,0 +1,10 @@
+package ch.zhaw.init.digitalfootprintexplorer.digitalfootprintexplorer.servicelayerplatform.model
+
+/**
+ * Stores information about an installed app.
+ */
+data class App(
+    val uid: Int,
+    val name: String,
+    val category: String
+)

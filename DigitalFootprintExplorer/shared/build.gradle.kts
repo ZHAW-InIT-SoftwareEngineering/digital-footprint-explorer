d95c34778c4728294ace3d8ab156e444a30e9bbd
Corinne Baumann <baumaco2@students.zhaw.ch>
2026-04-19 20:27:39 +0200

    fix: remove unused schemaVersion

--- DigitalFootprintExplorer/shared/build.gradle.kts
+++ DigitalFootprintExplorer/shared/build.gradle.kts
@@ -44,7 +44,6 @@ sqldelight {
     databases {
         create("DFEDatabase") {
             packageName.set("ch.zhaw.init.digitalfootprintexplorer.digitalfootprintexplorer.database")
-            schemaVersion = 2
         }
     }
 }
